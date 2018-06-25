0a4d1fd5b1b9751b1e3975ad51b6d54e443500ce
rsowell <rsowell@gmail.com>
2018-06-25 14:44:48 -0500

    initial stove commit

---
+++ Assets/CollisionBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBehavior : MonoBehaviour {
+	void OnCollisionEnter(Collision collision) {
+	    GameObject stove = gameObject.transform.parent.gameObject;
+	    Animator stoveAnimator = stove.GetComponent<Animator> ();
+	    stoveAnimator.SetTrigger ("doorPressed");
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Stove/Demo/Scripts/video_player_play.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class video_player_play : MonoBehaviour {
+	VideoPlayer v;
+	
+
+	// Use this for initialization
+	void Start () {
+		v = GameObject.Find("Video Player").GetComponent<VideoPlayer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void play() {
+		v.Play();
+	}
+}
